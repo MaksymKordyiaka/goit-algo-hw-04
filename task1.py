acad5b4e17d6aa8fa5a10c7e2fe0b42f2f57e3cb
Maksym <maxkord2003@gmail.com>
2024-02-01 17:19:39 +0200

    Commit

--- task1.py
+++ task1.py
@@ -6,3 +6,22 @@
 Ваше завдання - розробити функцію total_salary(path), яка аналізує цей
 файл і повертає загальну та середню суму заробітної плати всіх розробників.
 '''
+import re
+
+def total_salary(path):
+    try:
+        with open(path, 'r') as file:
+            lines = file.readlines()
+            tot_salary = 0
+            for line in lines:
+                numbers = re.findall(r'\d+', line)
+                tot_salary += int(numbers[0])
+            if len(lines) > 0:
+                average_salary = tot_salary / len(lines)
+                return (tot_salary, average_salary)
+            else:
+                return (0, 0)
+    except FileNotFoundError:
+        print('Файл відсутній або пошкоджений')
+
+result = total_salary('task1_salary.txt')
